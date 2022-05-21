96d7a04a6203ff3278a460b3a03d01aad36eff5a
Nitin Das <nitinhash1308@gmail.com>
2022-05-21 22:56:07 +0530

    :wrench: Added ticket creator

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 //click to open and close modal containner
+var uid = new ShortUniqueId();
 const addbtnSelector = document.querySelector(".add-btn");
 const modalSelector = document.querySelector(".modal-cont");
+const mainCont = document.querySelector(".main-cont");
+let isModalPresent = false;
 
 addbtnSelector.addEventListener("click",function(){
-    if(modalSelector.style.display=='flex')
+    if(isModalPresent)
     modalSelector.style.display="none";
     else
     modalSelector.style.display="flex";
+
+    isModalPresent=!isModalPresent;
 
 
 });
 
 const allColorBox = document.querySelectorAll(".priority-color");
-
+let activeColor = "black";
 // Adding borders to color selected
 allColorBox.forEach(function(colorBox){
     colorBox.addEventListener("click",function(){
@@ -21,5 +26,42 @@ allColorBox.forEach(function(colorBox){
 
         });
         colorBox.classList.add("active");
+        activeColor = colorBox.classList[1];
     });
 });
+
+// Adding a new ticket when enter is pressed on textbox
+const textAreaCont = document.querySelector(".text-area");
+modalSelector.addEventListener("keydown", function (e) {
+    let key = e.key;
+    if (key == "Shift") {
+    //   console.log(modalPriorityColor);
+      console.log(textAreaCont.value);
+      createTicket(activeColor, textAreaCont.value);
+      
+      
+        modalSelector.style.display = "none";
+        isModalPresent = false;
+        textAreaCont.value = "";
+        allColorBox.forEach(function (colorElem) {
+            colorElem.classList.remove("active");
+        })
+    }
+});
+
+// function to create ticket of specified color and data
+
+function createTicket(ticketColor, data, ticketId) {
+    let id = ticketId || uid();
+    let ticketCont = document.createElement("div"); //<div></div>
+    ticketCont.setAttribute("class", "ticket-cont");
+    ticketCont.classList.add(ticketColor);
+    ticketCont.innerHTML = `
+        <div class="ticket-color ${ticketColor} "></div>
+        <div class="ticket-id">${id}</div>
+        <div class="task-area">${data}</div>
+    `;
+    
+    mainCont.appendChild(ticketCont);
+}
+
